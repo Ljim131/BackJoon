867bec397d32140781b1b656090291ee61c88571
Jims <ljmin0131@gmail.com>
2023-07-27 23:48:08 +0900

    0727

---
+++ 7_5597.cpp
@@ -0,0 +1,20 @@
+/*
+#include <iostream>
+
+int main() {
+
+    int student[31] = { 0, };
+    int a;
+
+    for (int i = 0; i < 28; i++) {
+        std::cin >> a;
+        student[a] = 1;
+    }
+
+    for (int i = 1; i <= 30; i++) {
+        if (!student[i]) {
+            std::cout << i << '\n';
+        }
+    }
+    return 0;
+}*/
---
+++ 8_1152.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+#include <string>
+
+int main() {
+
+	std::string s;
+	int a = 1;
+
+	std::getline(std::cin, s);
+	if (s.length() == 1 && s[0] == ' ') {
+		std::cout << 0;
+		return 0;
+	}
+
+	for (int i = 1; i < s.length() - 1; i++) {
+		if (s[i] == ' ') a++;
+	}
+
+	std::cout << a;
+
+	return 0;
+}*/
