c2f1279f920fcf944c5784b9825bcdab4126c2e3
Jims <ljmin0131@gmail.com>
2023-07-28 15:46:05 +0900

    0828

---
+++ 10_10988.cpp
@@ -0,0 +1,21 @@
+/*
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+	std::string a, b;
+	std::cin >> a;
+	b = a;
+	
+	reverse(a.begin(), a.end());
+
+	if (a == b) { 
+		std::cout << "1";
+	}
+	else {
+		std::cout << "0";
+	}
+
+	return 0;
+}*/
---
+++ 9_2444.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+int main() {
+
+	int N = 0;
+	std::cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+
+		for (int j = N - i; j > 0; j--) {
+			std::cout << " ";
+		}
+
+		for (int j = 2 * i - 1; j > 0; j--) {
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+
+	for (int i = 1; i < N; i++) {
+
+		for (int j = 0; j < i; j++) {
+			std::cout << " ";
+		}
+
+		for (int j = 2 * (N - i) - 1; j > 0; j--) {
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}*/
