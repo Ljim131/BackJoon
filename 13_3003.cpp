92f51f4b962bb9de70964e4d1c67658e4fada3c9
Jims <ljmin0131@gmail.com>
2023-07-30 19:14:48 +0900

    0730

---
+++ 13_3003.cpp
@@ -0,0 +1,26 @@
+/*
+#include <iostream>
+
+int main( ) {
+
+	int king = 1;
+	int queen = 1;
+	int rook = 2;
+	int bishop = 2;
+	int knight = 2;
+	int pawn = 8;
+
+	int in_king, in_queen, in_rook, in_bishop, in_knight, in_pawn;
+
+	std::cin >> in_king >> in_queen >> in_rook >> in_bishop >> in_knight >> in_pawn;
+
+	std::cout << king - in_king << " ";
+	std::cout << queen - in_queen << " ";
+	std::cout << rook - in_rook << " ";
+	std::cout << bishop - in_bishop << " ";
+	std::cout << knight - in_knight << " ";
+	std::cout << pawn - in_pawn << " ";
+
+	return 0;
+}
+*/
---
+++ 14_25206.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+#include<string>
+
+int main() {
+	std::string subject; double avg = 0; float sum = 0;
+
+	double score[20];
+	float grade[20];
+
+	std::string input;
+	for (int i = 0; i < 20; ++i)
+	{
+		std::cin >> subject >> grade[i] >> input;
+		if (input == "A+") score[i] = 4.5;
+		else if (input == "A0") score[i] = 4.0;
+		else if (input == "B+") score[i] = 3.5;
+		else if (input == "B0") score[i] = 3.0;
+		else if (input == "C+") score[i] = 2.5;
+		else if (input == "C0") score[i] = 2.0;
+		else if (input == "D+") score[i] = 1.5;
+		else if (input == "D0") score[i] = 1.0;
+		else if (input == "F") score[i] = 0.0;
+		else if (input == "P") { score[i] = 0.0; grade[i] = 0; }
+		avg += (score[i] * grade[i]);
+		sum += grade[i];
+	}
+	avg = avg / sum;
+	std::cout << std::fixed;
+	std::cout.precision(6);
+	std::cout << avg;
+
+	return 0;
+}
+*/
